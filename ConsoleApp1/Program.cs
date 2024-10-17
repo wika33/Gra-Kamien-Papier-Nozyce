76fe017824da7cfd46b42e7a394068677475091f
wika33 <wiki11830@gmail.com>
2024-10-17 12:23:26 +0200

    gra  k p n

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,37 @@
 {
     static void Main(string[] args)
     {
+        Random rnd = new Random();
+        int wybor_komputera = rnd.Next(3);
 
+        Console.WriteLine("Witaj w grze kamien papier nozyce! Zmierz sie naszym komputerem!");
+        Console.Write("co wybierasz: kamien (k) papier (p) nozyce (n): "); 
+        string wybor_gracza = Console.ReadLine();
+
+        int wybor_gracza_int;
+        switch(wybor_gracza)
+        {
+            case "k":
+                {
+                    wybor_gracza_int = 0;
+                    break;
+                }
+            case "p":
+                {
+                    wybor_gracza_int = 1;
+                    break;
+                }
+            case "n":
+                {
+                    wybor_gracza_int = 2;
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("zla odpowiedz");
+                    break;
+
+                }
+        }
     }
 }
